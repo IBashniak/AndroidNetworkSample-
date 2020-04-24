f40a7b989d118ca2c5512e6a95734bf0fb90ed49
Igor <amigo@ukr.net>
2020-04-24 16:10:42 +0300

    improvement

--- app/src/main/java/com/example/mynetworkapplication/Network.kt
+++ app/src/main/java/com/example/mynetworkapplication/Network.kt
@@ -27,12 +27,13 @@ class RestApi {
 
     fun AsyncGetRequest(act: Activity) {
         val urlBuilder =
-            HttpUrl.parse(URL+"gifs/search").newBuilder()
+            HttpUrl.parse(URL + "gifs/search").newBuilder()
         urlBuilder.addQueryParameter("api_key", API_KEY)
+        urlBuilder.addQueryParameter("q", "Horse")
 
         val url = urlBuilder.build().toString()
         val request: Request = Request.Builder()
-            .url(url)
+            .url("https://api.giphy.com/v1/gifs/search?api_key=7gapkk4T9HUCWF2R1XANh8VnnAPifZ2k&q=Horse")
             .build()
         Log.d("AsyncGetRequest url=", url)
 
@@ -40,13 +41,38 @@ class RestApi {
         call.enqueue(object : Callback {
             //            @Throws(IOException::class)
             override fun onResponse(call: Call?, response: Response?) {
-                Log.d("AsyncGetRequest", "onResponse")
-                call?.toString()
+                Log.d("AsyncGetRequest", "body ${response?.body()?.string()}  ")
+                Log.d("AsyncGetRequest", "message ${response?.message().toString()}  ")
+                Log.d("AsyncGetRequest", "isSuccessful ${response?.isSuccessful.toString()}  ")
+                Log.d("AsyncGetRequest", "headers ${response?.headers().toString()}  ")
+                Log.d("AsyncGetRequest", "cacheResponse ${response?.cacheResponse().toString()}  ")
+
+                Log.d("AsyncGetRequest", "call ${call.toString()}  ")
+
+                val request: Request = Request.Builder()
+                    .url("https://api.giphy.com/v1/gifs/search?api_key=7gapkk4T9HUCWF2R1XANh8VnnAPifZ2k&q=Horse")
+                    .build()
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onResponse(call: Call?, response: Response?) {
+                        Log.d("AsyncGetRequest 2", response.toString())
+                        act.runOnUiThread {
+                            val responseText = act.findViewById<TextView>(R.id.responseTxt)
+                            val resp = response?.body()?.string()
+                            Log.d("AsyncGetRequest  RESP", "body ${resp}  ")
+                            responseText.text = resp
+                        }
+                    }
+
+                    override fun onFailure(call: Call?, e: IOException?) {
+                        Log.d("AsyncGetRequest onFailure", e.toString())
+                    }
+                })
+
                 act.runOnUiThread {
                     val responseText = act.findViewById<TextView>(R.id.responseTxt)
-                    responseText.text = call?.toString()
+                    responseText.text = response?.toString()
                     responseText.textSize = 26F
-                    Log.d("AsyncGetRequest", call?.toString())
+                    Log.d("AsyncGetRequest", response?.toString())
                 }
             }
 
